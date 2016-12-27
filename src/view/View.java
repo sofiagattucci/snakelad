ed203323c347fb13d130e6031de0062d829cf003
Gabriele Guerrini <gabriele.guerrini6@studio.unibo.it>
2016-12-27 22:18:52 +0100

    Add view interface.

---
+++ src/view/View.java
@@ -0,0 +1,19 @@
+package view;
+
+/**
+ * Interface for the View.
+ */
+public interface View {
+
+    /**
+     * It sets the dice value in play scene.
+     * @param value
+     *     The value to be set
+     */
+    void setDiceValue(int value);
+
+    /**
+     * It starts the application.
+     */ 
+    void start();
+}
