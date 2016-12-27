171f7b5c13e2b5ba20e6df28745276512e00bcc9
Gabriele Guerrini <gabriele.guerrini6@studio.unibo.it>
2016-12-27 23:02:58 +0100

    Create ViewImpl class. It implements View interface.

---
+++ src/view/ViewImpl.java
@@ -0,0 +1,19 @@
+package view;
+
+/**
+ * This is the main class of the view and implements the View Interface.
+ */
+public class ViewImpl implements View {
+
+    @Override
+    public void setDiceValue(final int value) {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+}
+
