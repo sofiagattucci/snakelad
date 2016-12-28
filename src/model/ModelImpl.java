12fb70d57db0d898b547a468d3279301fdfcae71
Nicolas Barilari <nicolas.barilari@studio.unibo.it>
2016-12-28 14:40:44 +0100

    Create ModelImpl class, it implements Model interface

---
+++ src/model/ModelImpl.java
@@ -0,0 +1,24 @@
+package model;
+
+/**
+ * This is the main class of the model and implements the Model Interface.
+ */
+public class ModelImpl implements Model {
+
+    @Override
+    public int getRandomNumber() {
+
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void newGame() {
+
+    }
+
+}
