a9ffb5761ac2c4a612f55ec25f3852d4b691761d
Gabriele Guerrini <gabriele.guerrini6@studio.unibo.it>
2017-01-20 17:37:37 +0100

    Add logo in the game icon

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -5,6 +5,7 @@ import java.awt.Toolkit;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import utilities.ImageLoader;
 
 /**
  * This class creates and initializes the main frame of the application. 
@@ -12,17 +13,21 @@ import javafx.stage.StageStyle;
 public class MainFrame extends Application {
 
     private static final String TITLE = "SnakeNLadder";
+    private static final String ICONE = "./res/Logo.png";
 
     @Override
     public void start(final Stage defaultStage) {
 
         defaultStage.initStyle(StageStyle.UNDECORATED);
         defaultStage.setTitle(TITLE);
+        defaultStage.getIcons().add(ImageLoader.get().getImage(ICONE));
+
         defaultStage.setHeight(Toolkit.getDefaultToolkit().getScreenSize().getHeight() * Dimension.SCREEN_H_PERC);
         defaultStage.setWidth(Toolkit.getDefaultToolkit().getScreenSize().getWidth() * Dimension.SCREEN_W_PERC);
         defaultStage.setMaximized(true);
         defaultStage.centerOnScreen();
         defaultStage.setResizable(false);
+
         defaultStage.setScene(Menu.getScene(defaultStage));
         defaultStage.show();
     }
