57ca2b13736b2cb4c89dc7a02da34a9876d3dc3c
Gabriele Guerrini <gabriele.guerrini6@studio.unibo.it>
2017-02-22 15:48:12 +0100

    Improve class Settings.

--- src/view/scenes/Settings.java
+++ src/view/scenes/Settings.java
@@ -2,10 +2,13 @@ package view.scenes;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import utilities.Language;
 import view.BasicButton;
 import view.Dimension;
 
@@ -18,8 +21,10 @@ public final class Settings extends BasicScene {
     private static final String TITLE = "Settings";
     private static final String BACK = "Back";
     private static final String PAWN_COLOR = "Change pawns' color";
-    private static final String LANGUAGE_MSG = "Change language";
-    private static final int TITLE_FONT = 60; 
+    private static final String LANGUAGE_MSG = "Change language: ";
+    private static final String DEFAULT_LANGUAGE = Language.ENG.name();
+    private static final int TITLE_FONT = 60;
+    private static final int FONT = 20;
     private static final double BOX_SPACING = BasicButton.getButtonHeight() / 3;
     private static final double Y_TITLE_TRANSLATE = -Dimension.SCREEN_H / 10;
 
@@ -28,17 +33,28 @@ public final class Settings extends BasicScene {
 
     private final Label title = new Label(TITLE);
     private final Button pawnColor = new BasicButton(PAWN_COLOR);
-    private final Button language = new BasicButton(LANGUAGE_MSG);
+    private final Label langLabel = new Label(LANGUAGE_MSG);
+    private final ComboBox<Language> languages = new ComboBox<>();
+    private final HBox langBox = new HBox(langLabel, languages);
     private final Button back = new BasicButton(BACK);
-    private final VBox box = new VBox(title, pawnColor, language, back);
+    private final VBox box = new VBox(title, pawnColor, langBox, back);
 
     private Settings() {
 
         this.getDefaultLayout().setCenter(this.box);
         this.box.setAlignment(Pos.CENTER);
         this.box.setSpacing(BOX_SPACING);
+
         this.title.setFont(new Font(TITLE_FONT));
         this.title.setTranslateY(Y_TITLE_TRANSLATE);
+
+        this.langBox.setAlignment(Pos.CENTER);
+        this.langLabel.setFont(new Font(FONT));
+        for (final Language elem: Language.values()) {
+            this.languages.getItems().add(elem);
+        }
+        this.languages.setPromptText(DEFAULT_LANGUAGE);
+
         this.back.setOnAction(e -> {
             settingStage.setScene(Menu.getScene(settingStage));
         });
