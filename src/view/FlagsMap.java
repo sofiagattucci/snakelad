60cb07802953ddfbe1998e391b415dbac3f31ffb
Gabriele Guerrini <gabriele.guerrini6@studio.unibo.it>
2017-02-23 19:43:35 +0100

    Add class FlagsMap and replaced combo box with flags images in setting scene.

---
+++ src/view/FlagsMap.java
@@ -0,0 +1,48 @@
+package view;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import utilities.Language;
+
+/**
+ * This class hold a map with the different types of flags(Images) and their path.
+ */
+public final class FlagsMap {
+
+    private static final String EN = "./res/LanguagesFlags/united_kingdom.png";
+    private static final String IT = "./res/LanguagesFlags/italy.png";
+    private static final String FR = "./res/LanguagesFlags/france.png";
+    private static final String ES = "./res/LanguagesFlags/spain.png";
+    private static final String DE = "./res/LanguagesFlags/germany.png";
+
+    private static final FlagsMap INSTANCE = new FlagsMap();
+    private final Map<Language, String> flagsPathMap = new HashMap<>();
+
+    private FlagsMap() {
+            this.flagsPathMap.put(Language.EN, EN);
+            this.flagsPathMap.put(Language.IT, IT);
+            this.flagsPathMap.put(Language.FR, FR);
+            this.flagsPathMap.put(Language.ES, ES);
+            this.flagsPathMap.put(Language.DE, DE);
+    }
+
+    /**
+     * Getter of this class unique instance.
+     * @return
+     *     This class unique instance
+     */
+    public static FlagsMap get() {
+        return INSTANCE;
+    }
+
+    /**
+     * Getter of the path to the flags that represents a language.
+     * @return
+     *     The maps that holds all the flags' images paths
+     */
+    public Map<Language, String> getMap() {
+        return Collections.unmodifiableMap(this.flagsPathMap);
+    }
+}
--- src/view/scenes/Settings.java
+++ src/view/scenes/Settings.java
@@ -1,16 +1,22 @@
 package view.scenes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import utilities.ImageManager;
 import utilities.Language;
+import utilities.Pair;
 import view.BasicButton;
 import view.Dimension;
+import view.FlagsMap;
 import view.LanguageStringMap;
 import view.ViewImpl;
 
@@ -28,6 +34,10 @@ public final class Settings extends BasicScene {
     private static final int FONT = 20;
     private static final double BOX_SPACING = BasicButton.getButtonHeight() / 3;
     private static final double Y_TITLE_TRANSLATE = -Dimension.SCREEN_H / 10;
+    private static final double SMALL_FLAG_H = BasicButton.getButtonHeight();
+    private static final double SMALL_FLAG_W = SMALL_FLAG_H * 1.8;
+    private static final double BIG_FLAG_H = BasicButton.getButtonHeight() * 1.5; 
+    private static final double BIG_FLAG_W = BIG_FLAG_H * 1.8;
 
     private static Stage settingStage;
     private static Settings settingsScene = new Settings();
@@ -35,10 +45,10 @@ public final class Settings extends BasicScene {
     private final Label title = new Label(LanguageStringMap.get().getMap().get(TITLE_KEY));
     private final Button pawnColor = new BasicButton(PAWN_COLOR);
     private final Label langLabel = new Label(LanguageStringMap.get().getMap().get(LANGUAGE_MSG_KEY));
-    private final ComboBox<Language> languages = new ComboBox<>();
-    private final HBox langBox = new HBox(langLabel, languages);
+    private final List<Pair<Language, ImageView>> flagList = new ArrayList<>();
+    private final HBox flagsBox = new HBox();
     private final Button back = new BasicButton(LanguageStringMap.get().getMap().get(BACK_KEY));
-    private final VBox box = new VBox(title, pawnColor, langBox, back);
+    private final VBox box = new VBox(title, pawnColor, langLabel, flagsBox, back);
 
     private Settings() {
 
@@ -49,23 +59,36 @@ public final class Settings extends BasicScene {
         this.title.setFont(new Font(TITLE_FONT));
         this.title.setTranslateY(Y_TITLE_TRANSLATE);
 
-        this.langBox.setAlignment(Pos.CENTER);
+        this.flagsBox.setAlignment(Pos.CENTER);
         this.langLabel.setFont(new Font(FONT));
-        for (final Language elem: Language.values()) {
-            this.languages.getItems().add(elem);
-        }
-        this.languages.setPromptText(ViewImpl.getDefaultLanguage().name());
-        this.languages.setOnAction(e -> {
-            ViewImpl.getObserver().setLanguage(this.languages.getValue());
-            this.updateLanguage();
-            Menu.getScene(settingStage).updateLanguage();
-            Instructions.getScene(settingStage).updateLanguage();
-            SetUpGame.getScene(settingStage).updateLanguage();
-        });
 
         this.back.setOnAction(e -> {
             settingStage.setScene(Menu.getScene(settingStage));
         });
+
+        for (final Language lang: Language.values()) {
+            this.flagList.add(new Pair<>(lang, ImageManager.get().getImageView(FlagsMap.get().getMap().get(lang))));
+        }
+        for (final Pair<Language, ImageView> elem: this.flagList) {
+            this.flagsBox.getChildren().add(elem.getSecond());
+            elem.getSecond().setFitHeight(SMALL_FLAG_H);
+            elem.getSecond().setFitWidth(SMALL_FLAG_W);
+            elem.getSecond().setOnMouseClicked(e -> {
+                ViewImpl.getObserver().setLanguage(elem.getFirst());
+                this.updateLanguage();
+                Menu.getScene(settingStage).updateLanguage();
+                Instructions.getScene(settingStage).updateLanguage();
+                SetUpGame.getScene(settingStage).updateLanguage();
+                for (final Pair<Language, ImageView> image: this.flagList) {
+                    image.getSecond().setFitHeight(SMALL_FLAG_H);
+                    image.getSecond().setFitWidth(SMALL_FLAG_W);
+                }
+                elem.getSecond().setFitHeight(BIG_FLAG_H);
+                elem.getSecond().setFitWidth(BIG_FLAG_W);
+            });
+        }
+        this.flagList.get(0).getSecond().setFitHeight(BIG_FLAG_H);
+        this.flagList.get(0).getSecond().setFitWidth(BIG_FLAG_W);
     }
 
     private void updateLanguage() {
