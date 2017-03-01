572cdd730de2266d3dd00b10b0fb574f33c7772b
Gabriele Guerrini <gabriele.guerrini6@studio.unibo.it>
2017-03-01 14:29:44 +0100

    Fixed small bug in pawns' starting position

--- src/view/ViewImpl.java
+++ src/view/ViewImpl.java
@@ -135,8 +135,8 @@ public class ViewImpl implements View {
     @Override
     public void setBoardSize(final int n) {
         Dimension.setPawnHeight(n);
-        playScene.resizePawns();
         playScene.getBoard().setSize(n);
+        playScene.resizePawns();
     }
 } 
 
