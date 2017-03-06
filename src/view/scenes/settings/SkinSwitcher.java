4984fe485dfb8c96d51d989331a4af3c1b9d0706
Gabriele Guerrini <gabriele.guerrini6@studio.unibo.it>
2017-03-06 17:28:43 +0100

    Improve skin switcher #2

--- src/view/scenes/settings/SkinSwitcher.java
+++ src/view/scenes/settings/SkinSwitcher.java
@@ -10,6 +10,8 @@ import javafx.scene.text.Font;
 import view.BasicButton;
 import view.ViewImpl;
 import view.scenes.Instructions;
+import view.scenes.game.MultiPlayerGameScenes;
+import view.scenes.game.SinglePlayerGame;
 import view.scenes.setup.SetUpGame;
 
 /**
@@ -40,18 +42,24 @@ public class SkinSwitcher {
             ViewImpl.getMenuScene().setSkin(Color.LIGHTBLUE);
             Instructions.getScene(ViewImpl.getAppStage()).setSkin(Color.LIGHTBLUE);
             SetUpGame.getScene(ViewImpl.getAppStage()).setSkin(Color.LIGHTBLUE);
+            SinglePlayerGame.getScene(ViewImpl.getAppStage()).setSkin(Color.LIGHTBLUE);
+            MultiPlayerGameScenes.get(ViewImpl.getAppStage()).setActiveSkin(Color.LIGHTBLUE);
         });
         this.lightSkin.setOnAction(e -> {
             ViewImpl.getSettingsScene().setSkin(Color.ANTIQUEWHITE);
             ViewImpl.getMenuScene().setSkin(Color.ANTIQUEWHITE);
             Instructions.getScene(ViewImpl.getAppStage()).setSkin(Color.ANTIQUEWHITE);
             SetUpGame.getScene(ViewImpl.getAppStage()).setSkin(Color.ANTIQUEWHITE);
+            SinglePlayerGame.getScene(ViewImpl.getAppStage()).setSkin(Color.ANTIQUEWHITE);
+            MultiPlayerGameScenes.get(ViewImpl.getAppStage()).setActiveSkin(Color.ANTIQUEWHITE);
         });
         this.darkSkin.setOnAction(e -> {
             ViewImpl.getSettingsScene().setSkin(Color.DARKMAGENTA);
             ViewImpl.getMenuScene().setSkin(Color.DARKMAGENTA);
             Instructions.getScene(ViewImpl.getAppStage()).setSkin(Color.DARKMAGENTA);
             SetUpGame.getScene(ViewImpl.getAppStage()).setSkin(Color.DARKMAGENTA);
+            SinglePlayerGame.getScene(ViewImpl.getAppStage()).setSkin(Color.DARKMAGENTA);
+            MultiPlayerGameScenes.get(ViewImpl.getAppStage()).setActiveSkin(Color.DARKMAGENTA);
         });
 
     }
