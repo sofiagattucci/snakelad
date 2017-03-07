ef7adae6d234fa02174a78eeb4571fe1500ce696
Gabriele Guerrini <gabriele.guerrini6@studio.unibo.it>
2017-03-07 13:54:16 +0100

    Improve Login class

--- src/view/scenes/Login.java
+++ src/view/scenes/Login.java
@@ -2,8 +2,10 @@ package view.scenes;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import view.BasicButton;
 import view.LanguageStringMap;
@@ -14,14 +16,18 @@ import view.dialogboxes.ClosureHandler;
  */
 public final class Login extends BasicScene {
 
-    private static final String QUIT_KEY = "menu.quit";
+    private static final String LABEL_KEY = "login.label";
     private static final String ENTER_KEY = "login.enter";
+    private static final String QUIT_KEY = "menu.quit";
+    private static final double BOX_SPACING = BasicButton.getButtonHeight() / 3;
+    private static final int FONT_SIZE = 30;
 
     private static Login loginScene = new Login();
     private static Stage loginStage;
 
     private final VBox box = new VBox();
     private final TextField nameField = new TextField();
+    private final Label descLabel = new Label(LanguageStringMap.get().getMap().get(LABEL_KEY));
     private final Button enter = new BasicButton(LanguageStringMap.get().getMap().get(ENTER_KEY));
     private final Button quit = new BasicButton(LanguageStringMap.get().getMap().get(QUIT_KEY));
     private final ClosureHandler closure = new ClosureHandler(loginStage);
@@ -30,7 +36,10 @@ public final class Login extends BasicScene {
 
         this.getDefaultLayout().setCenter(this.box);
         this.box.setAlignment(Pos.CENTER);
-        this.box.getChildren().addAll(this.nameField, this.enter, this.quit);
+        this.box.getChildren().addAll(this.descLabel, this.nameField, this.enter, this.quit);
+        this.box.setSpacing(BOX_SPACING);
+        this.box.setMaxWidth(BasicButton.getButtonWidth());
+        this.descLabel.setFont(new Font(FONT_SIZE));
         this.enter.setOnAction(e -> {
             loginStage.setScene(Menu.getScene(loginStage));
         });
--- src/view/scenes/settings/LanguageSwitcher.java
+++ src/view/scenes/settings/LanguageSwitcher.java
@@ -18,6 +18,7 @@ import view.BasicButton;
 import view.LanguageStringMap;
 import view.ViewImpl;
 import view.scenes.Instructions;
+import view.scenes.Login;
 import view.scenes.Menu;
 import view.scenes.setup.SetUpGame;
 
@@ -61,6 +62,7 @@ public class LanguageSwitcher {
             elem.getSecond().setOnMouseClicked(e -> {
                 ViewImpl.getSettingsScene().updateComboLang();
                 ViewImpl.getObserver().setLanguage(elem.getFirst());
+                Login.getScene(stage).updateLanguage();
                 Menu.getScene(stage).updateLanguage();
                 Instructions.getScene(stage).updateLanguage();
                 SetUpGame.getScene(stage).updateLanguage();
