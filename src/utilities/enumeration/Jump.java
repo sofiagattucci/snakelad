17d3e999534e8673e274dd8c2f3bf5b8ccfdcbb8
Nicolas Barilari <nicolas.barilari@studio.unibo.it>
2017-03-15 19:02:15 +0100

    Add enumeration Jump inside utilities.enumeration package

---
+++ src/utilities/enumeration/Jump.java
@@ -0,0 +1,18 @@
+package utilities.enumeration;
+
+/**
+ * Enumeration of type of jumps inside sceneries.
+ */
+public enum Jump {
+
+    /**
+     * The jump is caused by a snake.
+     */
+    SNAKE,
+
+    /**
+     * The jump is caused by a ladder.
+     */
+    LADDER;
+
+}
